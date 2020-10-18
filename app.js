85d1a0abebb0fb6ac5708302f9d3d2ad501f3a91
David-Ferrero1 <ferrero_d@hotmail.com>
2020-10-18 22:30:32 +0200

    Correction nombre de cycles ok avec reset

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ const affichagePause = document.querySelector('.affichageP');
 const btnGo = document.querySelector('.b1');
 const btnPause = document.querySelector('.b2');
 const btnReset = document.querySelector('.b3');
-const cycles = document.querySelector('h2')
+const cycles = document.querySelector('h2');
 
 // Les paramètres initiaux
 
 let checkInterval = false; // pour éviter de spammer le chiffter < 0
-let tempsInitial = 3; // 30min
-let tempsDeRepos = 3;  //  5min
+let tempsInitial = 1800; // 30min
+let tempsDeRepos = 300;  //  5min
 let pause = false; // car on ne commence pas avec une pause
 let nbDeCycles = 0;
 cycles.innerText = `Nombre de Cycles: ${nbDeCycles}.`; // infos utilisateur
 
 // Affichage dans bloc travail et pause
 
-affichageTravail.innerText = `${Math.trunc(tempsInitial/60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60}` : tempsInitial % 60}`; //Template litteral
-affichagePause.innerText = `${Math.trunc(tempsDeRepos/60)} : ${(tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60}`; //Template litteral
+affichageTravail.innerText = `${Math.trunc(tempsInitial/60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60}` : tempsInitial % 60}`; 
+affichagePause.innerText = `${Math.trunc(tempsDeRepos/60)} : ${(tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60}`; 
 
 // Activation des bouttons
 
@@ -39,10 +39,10 @@ btnGo.addEventListener('click', () => {
                 tempsInitial--;
                 affichageTravail.innerText = `${Math.trunc(tempsInitial/60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60}` : tempsInitial % 60}`;
             } else if (pause === false && tempsDeRepos === 0 && tempsInitial === 0) {
-                tempsInitial = 3;
-                tempsDeRepos = 3;
-                nbDeCycles = nbDeCycles + 1;
-                console.log(nbDeCycles);
+                tempsInitial = 1800;
+                tempsDeRepos = 300;
+                nbDeCycles++;
+                cycles.innerText = `Nombre de Cycles: ${nbDeCycles}.`;
                 affichageTravail.innerText = `${Math.trunc(tempsInitial/60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60}` : tempsInitial % 60}`;
                 affichagePause.innerText = `${Math.trunc(tempsDeRepos/60)} : ${(tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60}`;
 
@@ -60,6 +60,8 @@ btnGo.addEventListener('click', () => {
         checkInterval = false;
         tempsInitial = 1800;
         tempsDeRepos = 300;
+        nbDeCycles = 0;
+        cycles.innerText = `Nombre de Cycles: ${nbDeCycles}.`;
         affichageTravail.innerText = `${Math.trunc(tempsInitial/60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60}` : tempsInitial % 60}`;
         affichagePause.innerText = `${Math.trunc(tempsDeRepos/60)} : ${(tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60}`;
 
