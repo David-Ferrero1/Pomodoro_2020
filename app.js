4d3bc215a5352ddb80cdb65fc94519f1634b92db
David-Ferrero1 <ferrero_d@hotmail.com>
2020-10-18 21:45:39 +0200

    Gestion des actions sauf l'affichage du nombre de cycles pas ok, mais affichage à la console ok

--- app.js
+++ app.js
@@ -9,13 +9,42 @@ const cycles = document.querySelector('h2')
 
 // Les paramètres initiaux
 
-let tempsInitial = 1800; // 30min
-let tempsDeRepos = 300;  // 05min
+let tempsInitial = 3; // 30min
+let tempsDeRepos = 3;  //  5min
 let pause = false; // car on ne commence pas avec une pause
-let nbDeCycles = 0;
-cycles.innerText = `Nombre de Cycle: ${nbDeCycles}.`; // infos utilisateur
+let nbDeCycles= 0;
+cycles.innerText = `Nombre de Cycles: ${nbDeCycles}.`; // infos utilisateur
 
 // Affichage dans bloc travail et pause
 
 affichageTravail.innerText = `${Math.trunc(tempsInitial/60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60}` : tempsInitial % 60}`; //Template litteral
 affichagePause.innerText = `${Math.trunc(tempsDeRepos/60)} : ${(tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60}`; //Template litteral
+
+// Activation des bouttons
+
+btnGo.addEventListener('click', () => {
+
+    // initialitasation du timer 30 min
+    tempsInitial--;
+    affichageTravail.innerText = `${Math.trunc(tempsInitial/60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60}` : tempsInitial % 60}`;
+    console.log(nbDeCycles);
+    let timer = setInterval(() => {
+        
+        if(tempsInitial > 0) {
+            tempsInitial--;
+            affichageTravail.innerText = `${Math.trunc(tempsInitial/60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60}` : tempsInitial % 60}`;
+        } else if (tempsDeRepos === 0 && tempsInitial === 0) {
+            tempsInitial = 3;
+            tempsDeRepos = 3;
+            nbDeCycles = nbDeCycles + 1;
+            console.log(nbDeCycles);
+            affichageTravail.innerText = `${Math.trunc(tempsInitial/60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60}` : tempsInitial % 60}`;
+            affichagePause.innerText = `${Math.trunc(tempsDeRepos/60)} : ${(tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60}`;
+
+        } else if (tempsInitial === 0) {
+            tempsDeRepos--;
+            affichagePause.innerText = `${Math.trunc(tempsDeRepos/60)} : ${(tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60}`;
+        }
+
+    }, 1000)
+})
