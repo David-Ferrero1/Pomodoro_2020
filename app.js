79344bcd455616047a26c57e6098148f9aaa4c50
David-Ferrero1 <ferrero_d@hotmail.com>
2020-10-18 22:23:03 +0200

    Finitions ok, appli ok

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ const cycles = document.querySelector('h2')
 
 // Les paramètres initiaux
 
+let checkInterval = false; // pour éviter de spammer le chiffter < 0
 let tempsInitial = 3; // 30min
 let tempsDeRepos = 3;  //  5min
 let pause = false; // car on ne commence pas avec une pause
-let nbDeCycles= 0;
+let nbDeCycles = 0;
 cycles.innerText = `Nombre de Cycles: ${nbDeCycles}.`; // infos utilisateur
 
 // Affichage dans bloc travail et pause
@@ -24,27 +25,59 @@ affichagePause.innerText = `${Math.trunc(tempsDeRepos/60)} : ${(tempsDeRepos % 6
 
 btnGo.addEventListener('click', () => {
 
-    // initialitasation du timer 30 min
-    tempsInitial--;
-    affichageTravail.innerText = `${Math.trunc(tempsInitial/60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60}` : tempsInitial % 60}`;
-    console.log(nbDeCycles);
-    let timer = setInterval(() => {
-        
-        if(tempsInitial > 0) {
-            tempsInitial--;
-            affichageTravail.innerText = `${Math.trunc(tempsInitial/60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60}` : tempsInitial % 60}`;
-        } else if (tempsDeRepos === 0 && tempsInitial === 0) {
-            tempsInitial = 3;
-            tempsDeRepos = 3;
-            nbDeCycles = nbDeCycles + 1;
-            console.log(nbDeCycles);
-            affichageTravail.innerText = `${Math.trunc(tempsInitial/60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60}` : tempsInitial % 60}`;
-            affichagePause.innerText = `${Math.trunc(tempsDeRepos/60)} : ${(tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60}`;
+    //check interval ici
+    if(checkInterval === false) {
+        checkInterval = true;
+    
 
-        } else if (tempsInitial === 0) {
-            tempsDeRepos--;
-            affichagePause.innerText = `${Math.trunc(tempsDeRepos/60)} : ${(tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60}`;
-        }
+        // initialitasation du timer 30 min
+        tempsInitial--;
+        affichageTravail.innerText = `${Math.trunc(tempsInitial/60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60}` : tempsInitial % 60}`;
+        let timer = setInterval(() => {
+            
+            if(pause === false && tempsInitial > 0) {
+                tempsInitial--;
+                affichageTravail.innerText = `${Math.trunc(tempsInitial/60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60}` : tempsInitial % 60}`;
+            } else if (pause === false && tempsDeRepos === 0 && tempsInitial === 0) {
+                tempsInitial = 3;
+                tempsDeRepos = 3;
+                nbDeCycles = nbDeCycles + 1;
+                console.log(nbDeCycles);
+                affichageTravail.innerText = `${Math.trunc(tempsInitial/60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60}` : tempsInitial % 60}`;
+                affichagePause.innerText = `${Math.trunc(tempsDeRepos/60)} : ${(tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60}`;
 
-    }, 1000)
+            } else if (pause === false && tempsInitial === 0) {
+                tempsDeRepos--;
+                affichagePause.innerText = `${Math.trunc(tempsDeRepos/60)} : ${(tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60}`;
+            }
+
+        }, 1000)
+
+
+ // boutton reset
+    btnReset.addEventListener('click', () => {
+        clearInterval(timer);
+        checkInterval = false;
+        tempsInitial = 1800;
+        tempsDeRepos = 300;
+        affichageTravail.innerText = `${Math.trunc(tempsInitial/60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60}` : tempsInitial % 60}`;
+        affichagePause.innerText = `${Math.trunc(tempsDeRepos/60)} : ${(tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60}`;
+
+    })
+
+    } else {
+        return;
+    }
+
+   
+});
+
+btnPause.addEventListener('click', () => {
+
+    if (pause === false){
+        btnPause.innerText = "Play";
+    } else if (pause === true ){
+        btnPause.innerText = "Pause";
+    }
+    pause = !pause;
 })
